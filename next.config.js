e56bb4deaccdaff7830d6ff1508bd62db4f6245b
monoj <monojkumar57859893@gmail.com>
2023-10-12 12:54:48 +0600

    asdfsad

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // compiler: {
-  //   styledComponents: true
-  // }
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
 
